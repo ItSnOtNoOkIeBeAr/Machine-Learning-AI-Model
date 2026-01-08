421d87837928ceea12bf4593a2d63c09f031b850
Miqu3lla <javierjaypee8@gmail.com>
2026-01-08 14:02:59 +0800

    remove config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-"""
-Configuration file for API keys
-⚠️ IMPORTANT: This file is in .gitignore - NEVER upload to GitHub!
-"""
-
-# Your Gemini API key
-GEMINI_API_KEY = "AIzaSyAKXvDTk36-QrJbvay08HYyPf7rcMsLT78"
